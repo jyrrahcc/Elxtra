f38e2462b3b4fdeb222e6c6fe57b2aa14c028b1e
hanszzy <hcpurino@gmail.com>
2024-07-24 23:00:06 +0800

    Final Contact us

--- contactus.cpp
+++ contactus.cpp
@@ -316,37 +316,39 @@ int main() {
   </section>
 </section>
 
-    <!-- Message Us Section -->
-<section class="w-100 text-light cover4 animate fade-up">
-<section class="message-us">
-  <section class="darkener">
-    <section class="container py-5 d-flex gap-5 flex-column">
-      <section class="row">
-        <section class="col-sm-7 animate fade-left">
-          <h2 class="text-shadow">Message Us</h2>
-          <form>
-            <div class="mb-3">
-              <label for="name" class="form-label">Name</label>
-              <input type="text" class="form-control" id="name" placeholder="Your Name">
-            </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" placeholder="Your Email">
-            </div>
-            <div class="mb-3">
-              <label for="message" class="form-label">Message</label>
-              <textarea class="form-control" id="message" rows="5" placeholder="Your Message"></textarea>
-            </div>
-            <div class="d-flex justify-content-start">
-              <a class="btn m-3 btn-outline-primary text-light fw-bold border-light">Send</a>
-            </div>
-          </form>
+<!-- Message Us Section -->
+<section class="w-100 text-light animate fade-up">
+  <section class="cover4">
+ <section class="darkener w-100 h-100">
+      <section class="container py-5 message-us"">
+        <section class="row">
+          <section class="col-sm-7 animate fade-left">
+            <h2 class="text-shadow">Message Us</h2>
+            <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Your Name">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Your Email">
+              </div>
+              <div class="mb-3">
+                <label for="message" class="form-label">Message</label>
+                <textarea class="form-control" id="message" rows="5" placeholder="Your Message"></textarea>
+              </div>
+              <div class="d-flex justify-content-start">
+                <a class="btn m-3 btn-outline-primary text-light fw-bold border-light">Send</a>
+              </div>
+            </form>
+          </section>
         </section>
       </section>
     </section>
   </section>
 </section>
-</section>
+
+
     </main>
       <!-- Footer Section -->
       <footer>
